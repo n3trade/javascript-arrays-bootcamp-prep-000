f1508ebc9ec28c0be83a9ba65844218b47f67e89
Ian Pavlovic <ian_pavlovic@msn.com>
2017-11-03 14:52:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,37 +1,34 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element){
-  var element2 = [element, ...array]
-  return element2
+  return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
 function addElementToEndOfArray(array, element){
-  var element2 = [...array, element]
-  return element2
+  return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
 function accessElementInArray(array, index){
-  return array[2]
+  return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array.slice(1)
+  var newRemove = array.slice(1)
+  return newRemove
 }
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 1)
-  return array.slice(0, array.length - 1)
+  var endRemove = array.slice(0, array.length - 1)
+  return endRemove
 }
